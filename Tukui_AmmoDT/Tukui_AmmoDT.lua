c138860c06cdab67612eaf6b67e179aa487cf3c0
Hydra <ryanandrews135@gmail.com>
2022-11-08 12:37:00 -0700

    1.12

--- Tukui_AmmoDT/Tukui_AmmoDT.lua
+++ Tukui_AmmoDT/Tukui_AmmoDT.lua
@@ -11,13 +11,13 @@ local ThrownSubType = LE_ITEM_WEAPON_THROWN
 
 local OnEnter = function(self)
 	GameTooltip:SetOwner(self:GetTooltipAnchor())
-	
+
 	if GetInventoryItemID("player", 0) then
 		GameTooltip:SetInventoryItem("player", 0)
 	elseif GetInventoryItemID("player", 18) then
 		GameTooltip:SetInventoryItem("player", 18)
 	end
-	
+
 	GameTooltip:Show()
 end
 
@@ -31,21 +31,21 @@ end
 
 local Update = function(self)
 	local Count = 0
-	
+
 	if (GetInventoryItemID("player", 0) > 0) then -- Ammo slot
 		Count = GetInventoryItemCount("player", 0)
 	else
 		local ThrownSlotID = GetInventoryItemID("player", 18)
-		
+
 		if (ThrownSlotID and ThrownSlotID > 0) then -- Thrown weapons
 			local ItemSubType = select(7, GetItemInfo(GetInventoryItemID("player", 18)))
-			
+
 			if (ItemSubType and ItemSubType == ThrownSubType) then
 				Count = GetInventoryItemCount("player", 18)
 			end
 		end
 	end
-	
+
 	self.Text:SetFormattedText("%s%s:|r %s%s|r", DataText.NameColor, AMMOSLOT, DataText.ValueColor, Count)
 end
 
@@ -56,7 +56,7 @@ local Enable = function(self)
 	self:SetScript("OnEnter", OnEnter)
 	self:SetScript("OnLeave", OnLeave)
 	self:SetScript("OnMouseUp", OnMouseUp)
-	
+
 	self:Update()
 end
 
@@ -67,7 +67,7 @@ local Disable = function(self)
 	self:SetScript("OnEnter", nil)
 	self:SetScript("OnLeave", nil)
 	self:SetScript("OnMouseUp", nil)
-	
+
 	self.Text:SetText("")
 end
 
